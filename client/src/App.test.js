70cba178e1e11a507356927c4735a9eaff940b34
Stephen Freeman <stephen.freeman.dev@gmail.com>
2019-11-11 19:29:21 -0700

    testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
+import Navigation from "./components/Navigation";
 
 // it('renders without crashing', () => {
 //   const div = document.createElement('div');
@@ -12,9 +13,27 @@ import App from "./App";
 //   render(<App />);
 // });
 
-it("it displays text ", () => {
-  const simulatedDom = rtl.render(<App />);
-  // console.log(container.getByText("Soccer Players"));
-  const h1 = simulatedDom.queryByText(/Soccer Players/);
-  console.log(h1);
+// it("it displays text ", () => {
+//   const DomTest = rtl.render(<App />);
+//   // console.log(container.getByText("Soccer Players"));
+//   const h1 = DomTest.queryByText(/Soccer Players/);
+//   console.log(h1);
+// });
+
+describe("App", () => {
+  it("has a element with the text Soccer Player,", () => {
+    const DomTest = rtl.render(<App />);
+    const SprintTest = DomTest.getByText(/Soccer Player/i);
+    expect(SprintTest).toBeInTheDocument();
+    console.log(SprintTest);
+  });
+});
+
+describe("App", () => {
+  it("has a element with the text Soccer Player,", () => {
+    const DomTest = rtl.render(<App />);
+    const SprintTest2 = DomTest.getByText(/Stephen/i);
+    expect(SprintTest2).toBeInTheDocument();
+    console.log(SprintTest2);
+  });
 });
